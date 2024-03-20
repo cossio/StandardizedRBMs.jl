c5273e053e964acbe3b8d9fe7e4aa219509505ed
Jorge FdCD <jfdecd@icloud.com>
2024-03-20 17:51:19 +0000

    compat

--- src/constructors.jl
+++ src/constructors.jl
@@ -11,3 +11,17 @@ function BinaryStandardizedRBM(a::AbstractArray, b::AbstractArray, w::AbstractAr
     rbm = BinaryRBM(a, b, w)
     return standardize(rbm)
 end
+
+function SpinStandardizedRBM(
+    a::AbstractArray, b::AbstractArray, w::AbstractArray,
+    offset_v::AbstractArray, offset_h::AbstractArray,
+    scale_v::AbstractArray, scale_h::AbstractArray
+)
+    rbm = SpinRBM(a, b, w)
+    return StandardizedRBM(rbm, offset_v, offset_h, scale_v, scale_h)
+end
+
+function SpinStandardizedRBM(a::AbstractArray, b::AbstractArray, w::AbstractArray)
+    rbm = SpinRBM(a, b, w)
+    return standardize(rbm)
+end
